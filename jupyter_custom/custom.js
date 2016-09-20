88f2b9cf0d837812b669d3dc0cc6d5880272e727
Tudor Prodan <tprodan@kcg.com>
2016-09-20 11:41:13 -0400

    Updates for Centos7

--- jupyter_custom/custom.js
+++ jupyter_custom/custom.js
@@ -1,69 +1,26 @@
-// leave at least 2 line with only a star on it below, or doc generation fails
-/**
- *
- *
- * Placeholder for custom user javascript
- * mainly to be overridden in profile/static/custom/custom.js
- * This will always be an empty file in IPython
- *
- * User could add any javascript in the `profile/static/custom/custom.js` file
- * (and should create it if it does not exist).
- * It will be executed by the ipython notebook at load time.
- *
- * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
- *
- * Example :
- *
- * Create a custom button in toolbar that execute `%qtconsole` in kernel
- * and hence open a qtconsole attached to the same kernel as the current notebook
- *
- *    $([IPython.events]).on('app_initialized.NotebookApp', function(){
- *        IPython.toolbar.add_buttons_group([
- *            {
- *                 'label'   : 'run qtconsole',
- *                 'icon'    : 'icon-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
- *                 'callback': function () {
- *                     IPython.notebook.kernel.execute('%qtconsole')
- *                 }
- *            }
- *            // add more button here if needed.
- *            ]);
- *    });
- *
- * Example :
- *
- *  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
- *  to load custom script into the notebook.
- *
- *    // to load the metadata ui extension example.
- *    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
- *    // or
- *    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
- *    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
- *
- *
- * @module IPython
- * @namespace IPython
- * @class customjs
- * @static
- */
 
-$([IPython.events]).on("app_initialized.NotebookApp", function () {
-    $('div#header-container').toggle();
-    $('div#maintoolbar').toggle();
-    // IPython.layout_manager.do_resize();
+require(['base/js/namespace', 'base/js/events'], function(IPython, events) {
+    var hideHeaders = function(events) {
+        $('#header-container').toggle(); 
+        $('.header-bar').toggle();       
+        $('div#maintoolbar').toggle();
+        events.trigger('resize-header.Page');
+    };
 
-    // IPython.keyboard_manager.command_shortcuts.add_shortcut('Ctrl-k', 'ipython.move-selected-cell-up');
-    // IPython.keyboard_manager.command_shortcuts.add_shortcut('Ctrl-j', 'ipython.move-selected-cell-down');
+    var initNotebook = function() {
+        hideHeaders(events);
 
-    IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-k', function (event) {
-        IPython.notebook.move_cell_up();
-        return false;
-    });
+        IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-k', function (event) {
+            IPython.notebook.move_selection_up();
+            return false;
+        });
 
-    IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-j', function (event) {
-        IPython.notebook.move_cell_down();
-        return false;
-    });
+        IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-j', function (event) {
+            IPython.notebook.move_selection_down();
+            return false;
+        });
+    };
 
+    events.on('notebook_loaded.Notebook', initNotebook);
+    events.on('app_initialized.NotebookApp', initNotebook);
 });
